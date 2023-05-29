4b67d93517a90047c9ec601b597b3790a474e0a5
melkdesousa <melk.sousap@gmail.com>
2023-05-29 20:38:13 -0300

    use certs in server and client grpc

--- src/client-grpc.ts
+++ src/client-grpc.ts
@@ -1,11 +1,16 @@
-import { credentials } from '@grpc/grpc-js'
+import { credentials as ClientCredentials } from '@grpc/grpc-js'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 import { NoticeInfractionServiceClient } from './proto/notice-infraction_grpc_pb'
 import { Empty } from './proto/notice-infraction_pb'
 
-const client = new NoticeInfractionServiceClient(
-  'localhost:50051',
-  credentials.createInsecure(),
+const credentials = ClientCredentials.createSsl(
+  readFileSync(join(__dirname, '..', 'certs', 'ca.crt')),
+  readFileSync(join(__dirname, '..', 'certs', 'client.key')),
+  readFileSync(join(__dirname, '..', 'certs', 'client.crt')),
 )
+
+const client = new NoticeInfractionServiceClient('localhost:50051', credentials)
 
 client.list(new Empty(), (err, response) => {
   if (err) {
--- src/server-grpc.ts
+++ src/server-grpc.ts
@@ -1,15 +1,28 @@
 import { Server, ServerCredentials } from '@grpc/grpc-js'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 import { NoticeInfractionServiceService } from './proto/notice-infraction_grpc_pb'
 import { list } from './services/NoticeInfraction'
 
 const server = new Server()
+
+const credentials = ServerCredentials.createSsl(
+  readFileSync('./certs/ca.crt'),
+  [
+    {
+      cert_chain: readFileSync(join(__dirname, '..', 'certs', 'server.crt')),
+      private_key: readFileSync(join(__dirname, '..', 'certs', 'server.key')),
+    },
+  ],
+  true,
+)
 
 const main = () => {
   server.addService(NoticeInfractionServiceService, {
     list,
   })
 
-  server.bindAsync('0.0.0.0:50051', ServerCredentials.createInsecure(), () => {
+  server.bindAsync('0.0.0.0:50051', credentials, () => {
     server.start()
     console.log('Start gRPC server')
   })
